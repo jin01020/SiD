82c1b67342b0d13236be4d63ae131b26d1c6ac40
jin01020 <jin0.1020@gmail.com>
2025-07-30 08:00:56 +0900

    batch 80

--- run_sida.sh
+++ run_sida.sh
@@ -120,8 +120,8 @@ elif [ "$dataset" = 'imagenet64-cond' ]; then
     --alpha 1 \
     --tmax 800 \
     --init_sigma 2.5 \
-    --batch 40 \
-    --batch-gpu 10 \
+    --batch 80 \
+    --batch-gpu 20 \
     --data '/cvdata1/datasets/ImageNet/imagenet-64x64.zip' \
     --outdir '/131_data/jin0/cfm_sid/imagenet' \
     --resume '/131_data/jin0/cfm_sid/imagenet' \
